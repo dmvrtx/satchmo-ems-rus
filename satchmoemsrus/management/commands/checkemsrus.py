001b64a0ede46bcf08647a796e5998ee9ff19731
Valery Sukhomlinov <goodguy@goodguy.spb.ru>
2011-03-01 16:23:28 +0300

    Отработан выбор EMS локации по адресу

--- satchmoemsrus/management/commands/checkemsrus.py
+++ satchmoemsrus/management/commands/checkemsrus.py
@@ -10,5 +10,22 @@ class Command(BaseCommand):
     help = 'Checks EMS locations for contacts'
 
     def handle(self, *args, **options):
-        for contact in Contact.all():
-            with contact.shipping_address as address:
+        # сохраним все локации, чтобы не ходить по кругу каждый раз
+        for contact in Contact.objects.all():
+            address = contact.shipping_address
+            if address:
+                print 'ADDRESS: %r' % (address,)
+                print u'%s (%s)\t%s\t%s\t%s' % (
+                        address.country, address.country.iso2_code,
+                        address.state, address.city,
+                        self.find_location(address))
+
+    def find_location(self, address):
+        u"""Поиск локации EMS по адресу"""
+        if not hasattr(self, 'locations'):
+            self.locations = list(Location.objects.all().order_by('-kind'))
+        for location in self.locations:
+            if location.check_address(address):
+                return u'%s (%s)' % (location.name, location.kind)
+        return u'not found'
+
--- satchmoemsrus/models.py
+++ satchmoemsrus/models.py
@@ -7,10 +7,28 @@ from decimal import Decimal
 from django.db import models
 from django.utils.translation import get_language, ugettext_lazy as _
 from shipping.modules.base import BaseShipper
-import logging
 import datetime
+import logging
+import re
 
 log = logging.getLogger('satchmo-ems-rus')
+
+STRIP_JUNK = re.compile(r'[ -=+\r\n\t]')
+
+def is_similiar(str1, str2):
+    u"""Определение схожести строк"""
+    sub = STRIP_JUNK.sub('', str1.lower())
+    lookup = STRIP_JUNK.sub('', str2.lower())
+    while sub:
+        if lookup.find(sub) > -1:
+            break
+        if (len(sub) % 2):
+            sub = sub[:-1]
+        else:
+            sub = sub[1:]
+    if sub and ((float(len(sub)) / len (str1)) > 0.6):
+        return True
+    return False
 
 class Location(models.Model):
     u"""Представление области в которую может доставить EMS"""
@@ -25,19 +43,34 @@ class Location(models.Model):
     kind = models.CharField(_('Type'), max_length=1, choices=LOCATION_KINDS)
     name = models.CharField(_('Name'), max_length=128)
 
-    def in_contact(self, contact):
-        u"""Проверка данных контакта на соответствие местоположению"""
-        with contact.shipping_address as address:
-            if (self.kind == 'C') \
-                    and ((address.country.iso2_code == self.key) \
-                    or is_similiar(address.country.name, self.name)):
-                        return True
+    def check_address(self, address):
+        u"""Проверка данных адреса (класс AddressBook) на соответствие местоположению"""
+        if (self.kind == 'C') \
+                and ((address.country.iso2_code == self.key) \
+                or is_similiar(address.country.name, self.name)):
+                    return True
+        if (self.kind == 'R') \
+                and is_similiar(address.state, self.name):
+                    return True
+        if (self.kind == 'T') \
+                and is_similiar(address.city, self.name):
+                    return True
+        return False
 
 class Shipper(BaseShipper):
     u"""Метод отправки EMS"""
 
     def __str__(self):
         return 'EMS Russia shipper'
+
+    @property
+    def locations(self):
+        # обновим кэш, если пришло время
+        if (not hasattr(self, '_locations_expires')) \
+                or (datetime.datetime.now() > self._locations_expires):
+            self._locations = list(Location.objects.all().order_by('-kind'))
+            self._locations_expires = datetime.datetime.now() + datetime.timedelta(hours=1)
+        return self._locations
 
     def description(self):
         return _('Shipping by EMS Russia')
