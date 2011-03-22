5a92eb4cdbefab98318e5326ab6e5e8f9456134a
Valery Sukhomlinov <goodguy@goodguy.spb.ru>
2011-03-22 12:30:31 +0300

    Переименованы команды и добавлена команда получения регионов

---
+++ satchmo_emsrus/management/commands/emsrus_regions.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+u"""Команда замены списка российских регионов"""
+
+from django.core.management.base import BaseCommand, CommandError
+from urllib2 import urlopen
+from l10n.models import Country, AdminArea
+import json
+
+class Command(BaseCommand):
+    args = None
+    help = 'Replace Russian areas with those from EMS Russia API'
+
+    def clean_name(self, name):
+        return name.strip('* \t\r\n')
+
+    def handle(self, *args, **options):
+        u"""Замена списка областей России на EMS"""
+        country = Country.objects.filter(iso2_code='RU').all()
+        if len(country):
+            # Россию нашли
+            country = country[0]
+
+            # теперь регионы
+            response = json.load(urlopen('http://emspost.ru/api/rest/?method=ems.get.locations&type=regions&plain=true'))
+            # удаляем старые
+            regions = AdminArea.objects.filter(country=country).delete()
+            for loc in response['rsp']['locations']:
+                row = AdminArea(country=country, name=self.clean_name(loc['name']))
+                row.save()
+            print '%d region(s) retrieved' % (len(response['rsp']['locations']),)
+
